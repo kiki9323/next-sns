df7287b20376db6c045e6d012dfcf56a656cceb8
noodle <noodle>
2023-12-15 15:10:23 +0900

    (afterLogin) home/page.tsx

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,12 @@
+import Post from "../_component/Post";
+import PostForm from "./_component/PostForm";
+import Tab from "./_component/Tab";
+import TabProvider from "./_component/TabProvider";
+import style from "./home.module.css";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={style.main}>
+    </div>
+  );
 }
