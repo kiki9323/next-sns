8f2e89267819d53148a3d9788caa2e34bae3e72e
noodle <noodle>
2023-12-15 15:09:18 +0900

    (beforeLogin) 레이아웃 수정

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -8,7 +8,6 @@ type Props = {
 export default function Layout({ children, modal }: Props) {
   return (
     <div className={styles.container}>
-      (BeforeLogin의 Layout.tsx 입니다.)
       {children}
       {modal}
     </div>
